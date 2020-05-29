b41374d5c0b967c2646d71ea5b8d3c8d9225dfa0
aaronsawatsky <63100402+aaronsawatsky@users.noreply.github.com>
2020-05-29 13:46:37 +0900

    Add files via upload

---
+++ Clock/app.js
@@ -0,0 +1,27 @@
+function showTime() {
+    let currentTime = document.getElementById("time");
+    let currentDate = document.getElementById("date");
+    let today = new Date();
+    let day = today.getDay();
+    let month = today.getMonth();
+    let date = today.getDate();
+    let year = today.getFullYear();
+    let dayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let monthArr = ["January", "February", "March", "April", "May", "June", "July", "September", "October", "November", "December"];
+    let hours = today.getHours();
+    if (hours < 10) {
+        hours = "0" + hours;
+    };
+    let minutes = today.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    };
+    let seconds = today.getSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    };
+    currentDate.textContent = dayArr[day] + ", " + monthArr[month] + " " + date + ", " + year;
+    currentTime.innerHTML = hours + ":" + minutes + ":" + seconds;
+    setTimeout("showTime()", 1000);
+}
+showTime();
