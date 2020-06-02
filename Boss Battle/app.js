0384fc80215f5486cdf546a2ee356db8556e73ac
aaronsawatsky <63100402+aaronsawatsky@users.noreply.github.com>
2020-06-02 21:38:42 +0900

    Update app.js

--- Boss Battle/app.js
+++ Boss Battle/app.js
@@ -6,18 +6,39 @@ let bossHealth = 1000;
 let playerHealth = 1000;
 
 
+// Enemy Attack
+
+function enemyAttack() {
+    let bossAtk = Math.floor(Math.random() * 100) + 1;
+    let bossPercentage = Math.floor(Math.random() * 2) + 1;
+    if (`${playerHealth - bossAtk}` < 1) {
+        playerHP.innerHTML = "You Died";
+        document.getElementById("normAtk-btn").disabled = true;
+        document.getElementById("heavyAtk-btn").disabled = true;
+        document.getElementById("magicAtk-btn").disabled = true;
+    } else if (bossPercentage === 1) {
+        battleLog.innerHTML = "You dodged the attack";
+    } else {
+        battleLog.innerHTML = "Skate Punk shred fiercely on his skateboard for " + bossAtk + " damage!";
+        playerHP.innerHTML = `You : ${playerHealth = playerHealth - bossAtk} / 1000`;
+    }
+}
 
 // Attack option 1: Normal Attack
+
 function normAtk() {
     let normalAtk = Math.floor(Math.random() * 50) + 1;
     let normalPercentage = Math.floor(Math.random() * 5) + 1;
         if (`${bossHealth - normalAtk}` < 1) {
             bossHP.innerHTML = "You Defeated";
+            document.getElementById("normAtk-btn").disabled = true;
+            document.getElementById("heavyAtk-btn").disabled = true;
+            document.getElementById("magicAtk-btn").disabled = true;
         } else if (normalPercentage === 1) {
                 battleLog.innerHTML = "Miss!";
         } else {
             battleLog.innerHTML = "Normal Attack" + ": " + normalAtk;
-            bossHP.innerHTML = `${bossHealth = bossHealth - normalAtk} / 1000`;
+            bossHP.innerHTML = `Skate Punk : ${bossHealth = bossHealth - normalAtk} / 1000`;
         }
 }
 
@@ -28,11 +49,14 @@ function heavyAtk() {
     let heavyPercentage = Math.floor(Math.random() * 3) + 1;
         if (`${bossHealth - heavyAtk}` < 1) {
             bossHP.innerHTML = "You Defeated";
+            document.getElementById("normAtk-btn").disabled = true;
+            document.getElementById("heavyAtk-btn").disabled = true;
+            document.getElementById("magicAtk-btn").disabled = true;
         } else if (heavyPercentage === 1) {
             battleLog.innerHTML = "Miss!";
         } else {
             battleLog.innerHTML = "Heavy Attack" + ": " + heavyAtk;
-            bossHP.innerHTML = `${bossHealth = bossHealth - heavyAtk} / 1000`;
+            bossHP.innerHTML = `Skate Punk : ${bossHealth = bossHealth - heavyAtk} / 1000`;
         }
 }
 
@@ -43,25 +67,13 @@ function magicAtk() {
     let magicPercentage = Math.floor(Math.random() * 4) + 1;
         if (`${bossHealth - magicAtk}` < 1) {
         bossHP.innerHTML = "You Defeated";
+        document.getElementById("magicAtk-btn").disabled = true;
+        document.getElementById("heavyAtk-btn").disabled = true;
+        document.getElementById("normAtk-btn").disabled = true;
     } else if (magicPercentage === 1) {
         battleLog.innerHTML = "Miss!";
     } else {
         battleLog.innerHTML = "Magic Attack" + ": " + magicAtk;
-        bossHP.innerHTML = `${bossHealth = bossHealth - magicAtk} / 1000`;
+        bossHP.innerHTML = `Skate Punk : ${bossHealth = bossHealth - magicAtk} / 1000`;
     }
 }
-
-function endTurn() {
-    let bossAtk = Math.floor(Math.random() * 100) + 1;
-    let bossPercentage = Math.floor(Math.random() * 2) + 1;
-    if (`${playerHealth - bossAtk}` < 1) {
-        playerHP.innerHTML = "You Died"
-    } else if (bossPercentage === 1) {
-        battleLog.innerHTML = "You dodged the attack";
-    } else {
-        battleLog.innerHTML = "Radical Skateboarder inflicted " + bossAtk + " damage!";
-        playerHP.innerHTML = `${playerHealth = playerHealth - bossAtk} / 1000`;
-    }
-}
-
-setTimeout("endTurn()", 2000)
